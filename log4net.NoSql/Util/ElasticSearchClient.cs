58ca6c21ce5206d780df8435e1916e1612b46a2e
Ernesto Merino <teto02@gmail.com>
2014-03-25 20:38:14 +1100

    moving rest client to instance scope rather than per request

--- log4net.NoSql/Util/ElasticSearchClient.cs
+++ log4net.NoSql/Util/ElasticSearchClient.cs
@@ -11,29 +11,31 @@ namespace log4net.NoSql.Util
         private readonly string _host;
         private readonly int _port;
         private readonly string _baseUri;
+        private readonly RestClient _restClient;
         public ElasticSearchClient(string host, int port)
         {
             _host = host;
             _port = port;
             _baseUri = string.Format("http://{0}:{1}", host, port);
 
+            _restClient = new RestClient(_baseUri);
+
+
         }
 
         public void Insert(string index, string type, string log)
         {
-
-            var client = new RestClient(_baseUri);
             var request = new RestRequest(index + "/" + type);
 
             request.AddParameter("application/json; charset=utf-8", log, ParameterType.RequestBody);
             request.RequestFormat = DataFormat.Json;
             request.Method = Method.POST;
             request.AddBody(log);
-
+            
             try
             {
                 //dont care about response as if it fails should not throw any exceptions
-                client.ExecuteAsync(request, response => {});
+                _restClient.ExecuteAsync(request, response => { });
             }
             catch(Exception ex)
             {
